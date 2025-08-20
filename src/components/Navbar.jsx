45b382cc2e41d8e26d915078dc8e224fed859322
Moomina Sereefa <130589221+moomina12@users.noreply.github.com>
2025-08-21 00:02:30 +0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,51 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
-export default function Navbar() {
+export default function Navbar({ onClose }) {
   return (
-    <nav className="fixed top-0 left-0 h-full w-48 z-50 bg-red-600 bg-opacity-90 text-white font-bold px-6 py-8 flex flex-col items-center">
-      
+    <nav className="h-full w-64 bg-red-600 bg-opacity-95 text-white font-bold px-6 py-8 flex flex-col items-center">
       {/* Logo */}
-      <img src={logo} alt="Logo" className="w-20 h-20 mb-8 object-contain" />
+      <img
+        src={logo}
+        alt="Logo"
+        className="w-20 h-20 mb-8 object-contain"
+      />
 
       {/* Menu Links */}
       <div className="flex flex-col space-y-6 w-full">
-        <Link to="/" className="hover:underline text-center">Home</Link>
-        <Link to="/about" className="hover:underline text-center">About</Link>
-        <Link to="/services" className="hover:underline text-center">Services</Link>
-        <Link to="/contact" className="hover:underline text-center">Contact</Link>
+        <Link
+          to="/"
+          onClick={onClose}
+          className="hover:underline text-center"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          onClick={onClose}
+          className="hover:underline text-center"
+        >
+          About
+        </Link>
+        <Link
+          to="/services"
+          onClick={onClose}
+          className="hover:underline text-center"
+        >
+          Services
+        </Link>
+        <Link
+          to="/contact"
+          onClick={onClose}
+          className="hover:underline text-center"
+        >
+          Contact
+        </Link>
       </div>
     </nav>
   );
 }
+
 
 
 
